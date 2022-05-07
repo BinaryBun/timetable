b22fe1238cd691829b1e6b5358acb8d84a8dac6e
BinaryBun <chibishew2018@gmail.com>
2022-05-07 15:48:39 +0300

    templating the schedule

--- app/main.go
+++ app/main.go
@@ -6,10 +6,27 @@ import ("fmt"
 
 type data struct {
   Group string;
+  Mon [][]string;
+}
+
+func (d *data) set_data() {
+  d.Group = "211-331"
+  d.Mon = [][]string{
+                      {"9:00-10:30",
+                "https://online.mospolytech.ru/",
+                "lms",
+                "Дискретные структуры и компьютинг (Лекция)",
+                "Будылина Евгения Александровна, Набебин Алексей Александрович"},
+                      {"10:40-12:10",
+                "https://yandex.ru/maps/213/moscow/?ll=37.646215%2C55.704464&mode=poi&poi%5Bpoint%5D=37.646083%2C55.704282&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D1737978898&z=20",
+                "ав4607",
+                "Организационное и правовое обеспечение информационной безопасности (Лекция)",
+                "Пашина Алла Дмитриевна"}}
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-  d := data{"211-331"}
+  d := data{}
+  d.set_data()
   t, _ := template.ParseFiles("templace/index.html")
   t.Execute(w, d)
 }
