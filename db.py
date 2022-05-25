a9efd396ca4ab0eb5d8e5409c78c2d44008404c2
BinaryBun <chibishew2018@gmail.com>
2022-05-25 09:45:04 +0300

    normal css

--- db.py
+++ db.py
@@ -17,8 +17,8 @@ class DB():
                     req_1 = "insert into `table` (`group`, `day`, nuber, `time`, audit, `name`, prepod, `data`, map)"
                     for i in data[1:]:
                         req_2 = f'("{group}", "{day}", "-", "{i[0]}", "{i[1]}", "{i[2]}", "{i[3]}", "{i[4]}", "-")'
-                        # print(f"{req_1} value {req_2};")
-                        cursor.execute(f"{req_1} value {req_2};")
+                        print(f"{req_1} value {req_2};")
+                        #cursor.execute(f"{req_1} value {req_2};")
 
         except Error as e:
             return f"Error: {e}"
